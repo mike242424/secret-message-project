b1d682c06ad3e22343078838ac07efc3527b1e96
mike242424 <108441006+mike242424@users.noreply.github.com>
2022-09-05 08:56:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -25,24 +25,31 @@ let secretMessage = [
   "JavaScript",
 ];
 
+// Use an array method to remove the last string of the array
 console.log(secretMessage.length);
 secretMessage.pop();
 console.log(secretMessage.length);
 
+// Use an array method to add the words to and Program as separate strings to the end of the secretMessage array
 secretMessage.push("to", "Program");
 console.log(secretMessage);
 
+// Change the word easily to the word right by accessing the index and replacing it
 console.log(secretMessage.indexOf("easily"));
 secretMessage[7] = "right";
 console.log(secretMessage);
 
+// Use an array method to remove the first string of the array
 secretMessage.shift();
 console.log(secretMessage);
 
+// Use an array method to add the string Programming to the beginning of the array
 secretMessage.unshift("Programming");
 console.log(secretMessage);
 
+// Use an array method to remove the strings get, right, the, first, time,, and replace them with the single string know
 secretMessage.splice(6, 5, "know");
 console.log(secretMessage);
 
+// On one line, use console.log() and .join() to print the secret message as a sentence
 console.log(secretMessage.join(" "));
